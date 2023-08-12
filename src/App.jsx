8190809209bbcec279c94f8b4c626da4240ed643
akani28 <nietocevallos61@gmail.com>
2023-08-12 01:43:26 -0500

    Hacer la peticion y el componente location

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-
+import { useEffect } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
+  useEffect(()=>{
+    axios.get("https://rickandmortyapi.com/api/location/4")
+    .then(({data})=>console.log(data))
+    .catch((err)=> console.log(err))
+  },[])
  
   return (
     <>
